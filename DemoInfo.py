f7c77109edfdb40ebdf01d9dc5c198eb47737be0
tsablatzky <44913090+tsablatzky@users.noreply.github.com>
2018-12-11 22:40:03 -0500

    Update DemoInfo.py

--- DemoInfo.py
+++ DemoInfo.py
@@ -7,31 +7,31 @@ Created on Sun Dec  2 20:50:01 2018
 """
 print("Let's start by getting to know you a little bit.")
 
-Gender = input("Please select one: Male: 1, Female: 2. Nonbinary: 3 ")
-if Gender == "1":
+uGender = input("Please select one: Male: 1, Female: 2. Nonbinary: 3 ")
+if uGender == "1":
     bmrFemale = (655.0 + (4.35 * float(Weight)) + (4.7 * float(Height)) - 
     +(4.7 * float(Age))
     calorieIntake1 = bmrFemale * activityLevel  ### I'm getting an invalid syntax error around here. I'm sure it's 
     print(calorieIntake1)                       ### something obvious, I'm just not sure what to change.
-if Gender == "2":
+if uGender == "2":
     bmrMale = 66 + (6.23 * float(Weight)) + (12.7 * float(Height)) - 
     (6.8 * float(Height))
     calorieIntake2 = bmrMale * activityLevel
     print(calorieIntake2)      
- if Gender == "3": 
+ if uGender == "3": 
     ### working on doing the average.                  
 
-Age = input("Enter your age numberically: ")
+uAge = input("Enter your age numberically: ")
 # Error control make sure a number is entered
 try:
    val = int(Age)
 except ValueError:
    print("That's not a number!")
 
-Weight = input("What is your current weight in pounds? ")
+uWeight = input("What is your current weight in pounds? ")
 # Error control/make sure a number is entered
 try:
-   val = int(Weight)
+   val = int(uWeight)
 except ValueError:
    print("That's not a number!")
 
@@ -48,7 +48,7 @@ except ValueError:
    print("That's not a number!")
 H_inch += H_feet * 12
 
-Height = [H_inch, H_feet]
+uHeight = [H_inch, H_feet]
 
 User = []
-User = [Gender, Age, Weight, Height]
+User = [uGender, uAge, uWeight, uHeight]
